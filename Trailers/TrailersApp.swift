8a929cfcd7e3112d1a880bafa666ed1b0b9bb158
Pedro <prthomas808@gmail.com>
2024-01-11 01:56:14 -0500

    Initial Commit

---
+++ Trailers/TrailersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailersApp.swift
+//  Trailers
+//
+//  Created by Pedro Thomas on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TrailersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
